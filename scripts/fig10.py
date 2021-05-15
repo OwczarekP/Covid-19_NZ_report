2f570fe7c7b0ff613a6eec26a1826b5ad34ede5b
Patrycja Owczarek <patrycja.k.owczarek@gmail.com>
2021-05-15 10:32:40 +0200

    update report

--- scripts/fig10.py
+++ scripts/fig10.py
@@ -33,9 +33,11 @@ def create_plot(df):
     df['Task'] = df['year']
     df['Start'] = df['start']
     df['Finish'] = df['end']
+
     fig = FF.create_gantt(df, colors=color_dict, index_col='level', show_colorbar=True,
                           title='Alert levels in 2020-2021 in New Zealand', group_tasks=True,
                           bar_width=0.5)
+
     fig.layout.xaxis.tickformat = '%d-%m'
     fig.update_yaxes(autorange="reversed")
 
