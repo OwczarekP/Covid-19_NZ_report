15447a0a9b9ead4b0bad41b6f0f5cbedbb2d66e9
Patrycja Owczarek <patrycja.k.owczarek@gmail.com>
2021-05-15 14:34:30 +0200

    update report

--- scripts/fig11.py
+++ scripts/fig11.py
@@ -2,55 +2,29 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import linear_model
-import numpy as np
 import seaborn as sns
 
 
 def load_csv(path):
     """load_csv
     This function load the file from the path and save it as dataframe
-    Then, it convert the cases and alert level to the array format
 
     :param path: string to the csv file
-    :return: alert_array: array with the alert level number
-            cases_array: array with new day cases
+    :return: df: dataframe with the data
     """
     df = pd.read_csv(path)
     df["Date"] = df["Date"].apply(lambda x: x[2:7])
-    cases = np.array(list(df["New_cases"]))
-    alert = np.array(list(df["Alert_level"]))
-
-    cases_array = [[i] for i in cases]
-    alert_array = [[i] for i in alert]
-    return alert_array, cases_array
+    return df
 
 
-def linear_regression(x, y):
-    """linear regression
-    This function create linear regression between alert level and new cases
-    calculating its coefficient
-    and create regression plot
+def influence_alerts_cases(df):
+    """influence_alert_cases
+    This function read the data and return the plot
+    showing the alert level on daily new cases
 
-    :param x: the array with the alert level
-    :param y: the array with the daily new cases
-    :return: regression plot, regression coefficient
+    :param df: the dataframe with the data about new cases and alert level
+    :return:
     """
-    regr = linear_model.LinearRegression()
-    regr.fit(x, y)
-    predictor_y = regr.predict(x)
-    # plot output
-    sns.set(font_scale=1.1)
-    sns.set_style("whitegrid")
-    fig, ax = plt.subplots(figsize=(8, 6))
-    plt.scatter(x, y, color='black')
-    plt.plot(x, predictor_y, color='blue', linewidth=3)
-    plt.xlabel("Alert")
-    plt.ylabel("Number of daily cases")
-    plt.title(f"Influence of new cases on New Zealand COVID-19 alert level", y=1, x=0.5, fontsize=13)
-    return plt.savefig("../images/fig11.png"), print('Coefficients: \n', regr.coef_)
-
-def Influence_alerts_cases(df):
     sns.set_style("whitegrid")
     plt.subplots(figsize=(10, 6))
     plt.title(f"Influence of alerts on new cases", y=1, x=0.5, fontsize=18)
@@ -58,16 +32,12 @@ def Influence_alerts_cases(df):
     plt.ylabel("Number of cases")
     plt.xticks(rotation=15)
     sns.scatterplot(data=df, y="New_cases", x="Alert_level", color="black")
-    return plt.savefig("Influence_alerts_cases.png")
+    return plt.savefig("../images/fig11.png")
 
-# statistics to make table
-df_alert = df[["Alert_level", "Date"]]
-values_alerts = df_alert.value_counts()
-count_a = df["Alert_level"].value_counts()
 
 def main():
-    alert_array, cases_array = load_csv("../data/alert_cases.csv")
-    linear_regression(alert_array, cases_array)
+    df = load_csv("../data/alert_cases.csv")
+    influence_alerts_cases(df)
 
 
 if __name__=='__main__':
